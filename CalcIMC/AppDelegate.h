043666cfe529d0a4570d0698cd37a86d32cb8186
Guest User <Guest@iMac.local>
2015-02-06 13:13:00 -0600

    Initial Commit

---
+++ CalcIMC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalcIMC
+//
+//  Created by Guest User on 2/6/15.
+//  Copyright (c) 2015 Tec de Monterrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalcIMC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CalcIMC
+//
+//  Created by Guest User on 2/6/15.
+//  Copyright (c) 2015 Tec de Monterrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
